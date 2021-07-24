cec1495d93cdbe09f819bef5b8a7b2e9a03ab6e8
rudyalvaradobeltran <rudy.e.alvarado.b@gmail.com>
2021-07-24 19:22:05 -0400

    Video streaming

--- ejemplos_express/miApp/app.js
+++ ejemplos_express/miApp/app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs, { stat, createReadStream } = require('fs');
+var { promisify } = require('util');
+
+const fileInfo =  promisify(stat);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,9 +24,76 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static('files'));
+
+app.get('/descarga/:nombre_usuario', (req, res) => {
+  const streamEscritura = fs.createWriteStream(`${__dirname}/files/text2.txt`);
+  streamEscritura.write(
+    'estimado '+req.params.nombre_usuario+' aquí está el documento que solicitas',
+    () => {
+      res.download(`${__dirname}/files/text2.txt`, error => {
+        if (error) {
+          console.log('ERROR');
+          res.status(404).render('error');
+        } else {
+          console.log('Descarga ok');
+        }
+      });
+    }
+  )
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/ejemplo', ejemploRouter);
+
+app.use('/video-static', (req, res, next) => {
+  const fileName = `${__dirname}/public/video/video.mp4`;
+  res.type('video/mp4');
+  res.sendFile(fileName);
+});
+
+app.use('/video-stream', (req, res, next) => {
+  const fileName = './public/video/video.mp4';
+  res.writeHead(200, {
+    "Content-Type": "video/mp4"
+  });
+
+  createReadStream(fileName).pipe(res);
+});
+
+app.use('/video-rango', async (req, res, next) => {
+  const fileName = './public/video/video.mp4';
+  const { size } = await fileInfo(fileName);
+  const range = req.headers.range;
+
+  if (range) {
+    let [start, end] = range.replace(/bytes=/, '').split('-');
+    start = parseInt(start, 10);
+    end = end ? parseInt(end, 10) : size - 1;
+    res.writeHead(206, {
+      "Content-Type": "video/mp4",
+      "Content-Length": end - start + 1,
+      "Accept-Ranges": "bytes",
+      "Content-Range": `bytes ${start}-${end}/${size}`
+    });
+    createReadStream(fileName, {start, end }).pipe(res);
+  } else {
+    res.writeHead(200, {
+      "Content-Type": "video/mp4",
+      "Content-Length": size
+    });
+    createReadStream(fileName).pipe(res);
+  }
+});
+
+app.use((req, res, next) => {
+  var currentURL = req.originalUrl;
+  if(currentURL === '/antiguo-documento') {
+    return res.redirect(301, 'https://www.google.com');
+  }
+  return next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- ejemplos_express/miApp/routes/users.js
+++ ejemplos_express/miApp/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+function sumar() {
+  return 1+1;
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  if(sumar() === 3) {
+    res.send('respond with a resource');
+  } else {
+    res.status(500);
+    res.render('error');
+  }
 });
 
 module.exports = router;
